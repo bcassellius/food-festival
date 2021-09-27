7381aa8ec991c36c93165f0d2ce45fbc2d55f410
Bri <bhilliker@gmail.com>
2021-09-27 16:28:56 -0500

    split application into modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -12,6 +14,9 @@ module.exports = {
           $: "jquery",
           jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+          })
     ],
     mode: 'development'
 };
